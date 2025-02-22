5e9018c30a90cb00b9fa6d8e1ac0677c69da844b
Vidhan27 <vidhanprajapati27@gmail.com>
2025-02-22 16:49:25 +0530

    Requirement and Flask update

--- Flask.py
+++ Flask.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import pickle
 import numpy as np
 import pandas as pd
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -138,4 +139,5 @@ def health_check():
     })
 
 if __name__ == '__main__':
-    app.run()
+    PORT=os.environ.get('PORT', 5000)
+    app.run(host='0.0.0.0', port=PORT)
